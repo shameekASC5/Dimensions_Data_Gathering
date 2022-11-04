0c39c59307fc0287abbd89944c32148c398966d8
Shameek Hargrave <shargraveseeds@gmail.com>
2022-11-04 10:40:21 -0400

    updated last ids from cloud

---
+++ scripts/store_data.sh
@@ -0,0 +1,7 @@
+cd ~
+
+# zip the data
+zip -r ccc_crawler.zip "Dimensions_Data_Gathering/data"
+zip -r logs.zip "Dimensions_Data_Gathering/logs"
+zip -r config.zip "Dimensions_Data_Gathering/config"
+
