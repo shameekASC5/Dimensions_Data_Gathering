4fbfac62ab4194fc95fcbb582b095962ea38b576
Shameek Hargrave <41080232+shameekASC5@users.noreply.github.com>
2022-11-04 10:53:29 -0400

    stored all zips in one folder

--- scripts/store_data.sh
+++ scripts/store_data.sh
@@ -5,3 +5,7 @@ zip -r ccc_crawler.zip "Dimensions_Data_Gathering/data"
 zip -r logs.zip "Dimensions_Data_Gathering/logs"
 zip -r config.zip "Dimensions_Data_Gathering/config"
 
+# move to zip file
+mv ccc_crawler.zip logs_config_full_data/
+mv logs.zip logs_config_full_data/
+mv config.zip logs_config_full_data/
